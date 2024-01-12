57564a884afe83dd79065555699ac6afe2168e1c
Gennady Terekhov <ter.ge@yandex.ru>
2024-01-12 21:54:45 +0400

    issue-59 fixed test coverage

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}],
+        ['@babel/preset-env', { targets: { node: 'current' } }],
         '@babel/preset-typescript',
     ],
     plugins: [
         'babel-plugin-transform-typescript-metadata',
-    ]
-};
+    ],
+}
