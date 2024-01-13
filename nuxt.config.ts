7f57ed1bd50b4e30846416c6c59e933bdc27a487
Gennady Terekhov <ter.ge@yandex.ru>
2024-01-13 22:48:18 +0400

    issue-85 updated web ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import typescript from '@rollup/plugin-typescript'
 import { defineNuxtConfig } from 'nuxt/config'
-import {Plugin} from "rollup";
+import { type Plugin } from 'rollup'
 
 const tsPlugin: Plugin = typescript()
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  // builder: 'webpack',
-
-  srcDir: './src/ui/web',
-  vite: {
-    plugins: [tsPlugin],
-  },
+    devtools: { enabled: true },
+    modules: [
+        'nuxt-quasar-ui',
+    ],
+    quasar: { /* */ },
+    srcDir: './src/ui/web',
+    vite: {
+        plugins: [tsPlugin],
+    },
 })
--- src/ui/web/pages/index.vue
+++ src/ui/web/pages/index.vue
@@ -1,32 +1,81 @@
 <template>
-    <div>
+    <div class="page-container">
         <div>
-            <h1>Welcome to Tribal Relations</h1>
-        </div>
-        <div
-            class="button"
-            @click="onStartClick"
-        >
-            start game button
+            <h1>Tribal Relations</h1>
         </div>
         <div>
-            rules
+            <h6 class="text-weight-light">
+                This is a boardgame about tribes and their relations. Tribes strive to conquer Rome.
+                Whoever conquers the first, wins.
+            </h6>
+        </div>
+        <div class="">
+            <div class="container">
+                <q-btn
+                    color="secondary"
+                    label="Start 🚀"
+                    @click="onStartClick"
+                />
+                <q-btn
+                    :label="`${areRulesShown ? 'Hide':'Show'} rules 📜`"
+                    class="button"
+                    @click="onRulesClick"
+                />
+                <div>
+                    <div v-if="areRulesShown">
+                        <q-card>
+                            <q-card-section>
+                                You play as a tribe. To win, you must conquer the city in the center of the map.
+                                Other tribes will attempt to do the same.
+                                You can ally with them or oppose them.
+                                Choose wisely!
+                                The game will block any impossible action. Try and see if you can learn the rules by
+                                playing!
+                            </q-card-section>
+                        </q-card>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-
 import { container } from '../../../NaiveDiContainer.ts'
 import TribalRelationsGame from '../../../outer/TribalRelationsGame'
 
 export default {
+    data() {
+        return {
+            areRulesShown: false,
+        }
+    },
+    mounted() {
+        document.title = 'Tribal Relations'
+    },
     methods: {
         onStartClick() {
-            const trGame = container.resolve(TribalRelationsGame)
+            const trGame = container.resolveSafely(TribalRelationsGame)
             trGame.startBrowser()
+        },
+        onRulesClick() {
+            this.areRulesShown = !this.areRulesShown
         },
     },
 }
 
 </script>
+<style scoped>
+.page-container {
+    margin: 100px;
+}
+
+.container {
+    display: flex;
+    margin: 20px;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 20px;
+    align-items: center;
+}
+</style>
